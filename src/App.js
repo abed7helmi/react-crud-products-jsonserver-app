41c8f5f50c630ed789bce628d4924dcf69c6271d
abed7helmi <abed.helmi@yahoo.fr>
2023-12-18 15:48:44 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import Products from "./components/Products";
+
+// pour utiliser bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return(
+      <BrowserRouter>
+        <nav className="m-1 p-1 border border-info">
+          <ul className="nav nav-pills">
+            <li>
+              <Link className="btn btn-outline-info ms-1" to={"/Home"}>Home</Link>
+            </li>
+            <li>
+              <Link className="btn btn-outline-info ms-1" to={"/Products"}>Products</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/Home" element={<Home/>}></Route>
+          <Route path="/Products" element={<Products/>}></Route>
 
+        </Routes>
+      </BrowserRouter>
   );
 }
 
